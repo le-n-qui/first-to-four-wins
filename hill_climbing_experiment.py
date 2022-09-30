49a6f6b75949f5bfb40d5d5c2471d5f96de9278b
Jason Huynh <63384364+TheNMEArengee@users.noreply.github.com>
2022-09-29 18:33:51 -0700

    added coin flipping number generation method

--- hill_climbing_experiment.py
+++ hill_climbing_experiment.py
@@ -31,11 +31,33 @@ def random_grid():
         for col_pos in range(COL_SIZE):
             # Fill in at each column position 
             # with a random value
-            row.append(random.randint(1, 5))
+            # row.append(random.randint(1, 5))
+            row.append(generate_value())
         # place the filled row into grid
         grid.append(row)
 
     return grid
+
+
+
+# flip an unbiased coin, if its heads treat it as a 1, 
+# it is tails, flip the coin again and if its heads treat it as a 2, 
+# if its tails flip the coin again ... -- If you get five tails in a row, 
+# restart the procedure
+def generate_value():
+    if(random.randint(0, 1) == 1):
+        return 1
+    elif(random.randint(0, 1) == 1):
+        return 2
+    elif(random.randint(0, 1) == 1):
+        return 3
+    elif(random.randint(0, 1) == 1):
+        return 4
+    elif(random.randint(0, 1) == 1):
+        return 5
+    else:
+        return generate_value()
+
 
 print('GRID STATUS: ')
 print('-----------')
