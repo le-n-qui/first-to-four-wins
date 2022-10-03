71b4dec39fd8dc005d40da657227204820cd83c0
Andy Le <andy.quile@gmail.com>
2022-10-03 12:30:45 -0700

    Get board_progress function to display board as desired

--- connect4_spin.py
+++ connect4_spin.py
@@ -3,15 +3,27 @@
 ROW_SIZE = 8
 COL_SIZE = 5
 
+board_dict = {1: 'a', 2: 'b', 3: 'c',
+			4: 'd', 5: 'e', 6: 'f', 
+			7: 'g', 8: 'h'}
+
 def board_progress():
 	# corners in one horizontal border
-	corners = ['+'] * (COL_SIZE+1)
-	# this is a representation of the board squares
-	board_squares = [ ['E'] * COL_SIZE ] * ROW_SIZE 
-	for pos in range(ROW_SIZE):
-		print('|'.join(board_squares[pos]))
-		print('-'.join(corners))
+    corners = ['+'] * (COL_SIZE+1)
+	# below is a representation of the board squares
+    board_squares = [ ['E'] * COL_SIZE ] * ROW_SIZE 
+    # print top margin
+    # 2 spaces +  numbers separated by space
+    print('  ' + ' '.join([ str(num) for num in range(1,COL_SIZE+1) ]))
+    for pos in range(ROW_SIZE):
+    	# letter on left margin + bar + square status separate by bar + bar
+        board_row = board_dict[pos+1] + '|' + '|'.join(board_squares[pos]) + '|'
+        print(board_row)
+        # print horizontal border
+        print(' ' + '-'.join(corners))
 
+# Ask human: which player, Red or Yellow, do they want to play as?
+# save input into player variable
 player = input("Which player would you like to play (R/Y)? ")
 
 board_progress()
